d7a3c788d629b3c7be5eb581a593981ae5ff272f
Rosario Ortiz <pochimoshy@gmail.com>
2022-05-12 10:00:48 -0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from "react";
+import {TodoCounter} from "./TodoCounter.js"
+import {TodoSearch} from "./TodoSearch.js";
+import {TodoList} from "./TodoList.js";
+import {TodoItem} from "./TodoItem.js";
+import {CreateTodoButton} from "./CreateTodoButton.js";
+import WebFont from 'webfontloader';
 import './App.css';
 
+const todos=[
+  {text:'Cortar cebolla', completed:false},
+  {text:'Tomar el curso de intro de react', completed:false},
+  {text:'Cortar Llorar con la llorona', completed:false},
+];
+
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Droid Sans', 'Chilanka', 'Lato']
+      }
+    });
+   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="Container">
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+      </div>
+    </React.Fragment>
   );
 }
+
+
 
 export default App;
---
+++ src/colors/Colors.js
@@ -0,0 +1,42 @@
+//* B&W
+export const black = '#212832';
+export const blackMenu = '#181314';
+export const blackReng = '#212832';
+export const blackForced = '#000';
+export const gray = '#7A7E84';
+export const menuOff = '#878A90';
+export const grayAlt = '#B2B4B7';
+export const grayAlert = '#1F1F1F';
+export const longText = '#555555';
+export const whiteAlt = '#F5F6F6';
+export const white = '#FDFFFE';
+export const grayClear = '#E0E0E0';
+export const grayForm = '#F2F2F2';
+
+//* Color
+export const purple = '#7F47DD';
+export const purpleAlt = '#9657FF';
+export const yellow = '#FBAE17';
+export const orange = '#FF6A00';
+export const orangeCool = '#F25A1D';
+export const magenta = '#FF00FF';
+export const voted = '#c8c1f1';
+export const red = '#F50F0F';
+export const redHeader = '#E50F0F';
+export const violetAlt = '#BE68D5';
+export const aqua = '#00A99D';
+export const blue = '#0071BC';
+export const blueAlt = '#3D28B1';
+export const wine = '#A61068';
+
+//*Brands
+export const spotify = '#1DB954';
+export const voxpop = '#3D28B1';
+export const bgVoxplayerA = '#003B80';
+export const bgVoxplayerB = '#0070F2';
+export const facebook = '#1877F2';
+export const google = '#2774E8';
+export const instagram = '#E1306C';
+export const twitter = '#1DA1F2';
+export const youtube = '#FF0000';
+export const whatsapp = '#25D366';
